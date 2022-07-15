48e2b748de0b51373099841f6e0cd0a46c378912
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-07-15 17:18:44 +0200

    Populated the Solver class
    This class owns the linear system and boundary condition data. Other helper functions needed to solve the linear system are also available in this class.

--- lib/sgi/Solver.py
+++ lib/sgi/Solver.py
@@ -1,22 +1,94 @@
-
 import numpy as np
+import sys
 
-class NewtonSolver:
+class NewtonRaphsonSolver:
 
-    def __init__(self, assembler):
-        self.assembler = assembler
-        self.Nmax = 20
-        # the matrix
-        self.A = np.empty([assembler.nequations, assembler.nequations])
-        # the right hand side 
-        self.f = np.empty([assembler.nequations])
+    def __init__(self, system):
+        self.system = system
+        # the left hand side matrix
+        self.A = np.zeros([system.nequations, system.nequations])
+        # the right hand side vector
+        self.f = np.zeros([system.nequations, 1])
+        # the solution (unknowns)
+        self.solution = np.zeros([system.nequations, 1])
+        # initialize the unknowns in the system
+        self.initialize()
+        # boundary condition types (0 = Dirichlet, 1 = Neumann) matrix
+        self.bctypes = np.ones([self.system.weak_form.function_space.N, self.system.weak_form.function_space.dof], dtype=np.int64)
+        # boundary condition values matrix
+        self.bcvalues = np.zeros([self.system.weak_form.function_space.N, self.system.weak_form.function_space.dof])
 
-    def solve(self):
+    # Function to initialize unknowns in the system to the undeformed state of the beam
+    def initialize(self):
+        # assuming initially straight beams are along the x-axis!!!
+        nodes_x = self.system.weak_form.function_space.nodes[:, 0:1]
+        dofs = self.system.weak_form.function_space.dof
+        for i in range(0, nodes_x.shape[0]):
+            self.solution[dofs*i:(dofs*i)+1, :] = nodes_x[i:i+1, :]
+        pass
+    
+    # Function to reset the linear system
+    def reset_system(self):
+        self.A = np.zeros([self.system.nequations, self.system.nequations])
+        self.f = np.zeros([self.system.nequations, 1])
+        pass
 
-        # nonlinear iterations
-        for i in range(0, self.Nmax):
+    # Create arrays with entries being the global dof numbers of Dirichlet and Neumann dofs
+    def create_dof_arrays(self):
+        bctypes_vec = np.reshape(self.bctypes, [self.system.nequations, 1])
+        global_dofs = np.arange(0, self.system.nequations, 1)
+        Dirichlet_dofs = global_dofs[(bctypes_vec == 0).flatten()]
+        Neumann_dofs = global_dofs[(bctypes_vec == 1).flatten()]
+        return Dirichlet_dofs, Neumann_dofs
+    
+    # Function to apply the Dirichlet boundary conditions by static condensation
+    def apply_static_condensation(self, Neumann_dofs):
+        self.A = self.A[np.ix_(Neumann_dofs, Neumann_dofs)]
+        self.f = self.f[Neumann_dofs]
+    
+    def solve(self, Nmax = 10):
+        # create the Dirichlet and Neumann global dof arrays
+        Dirichlet_dofs, Neumann_dofs = self.create_dof_arrays()
+        # If Dirichlet boundary conditions are not available
+        if (Dirichlet_dofs.size == 0):
+            sys.exit("\nDirichlet boundary conditions not found.")
+        else:
+            # generate the nodal load vector
+            nodal_loads = np.zeros([self.system.nequations, 1])
+            nodal_loads[Neumann_dofs] = np.reshape(self.bcvalues, [self.system.nequations, 1])[Neumann_dofs]
+            # generate the Dirichlet solution vector
+            Dirichlet_solution = np.reshape(self.bcvalues, [self.system.nequations, 1])[Dirichlet_dofs]
+            # add the Dirichlet solution to the overall solution vector
+            self.solution[Dirichlet_dofs] += Dirichlet_solution
+        # Newton-Raphson iterations
+        for i in range(0, Nmax):
             # assemble the linear system
-            self.assembler.assemble(self.A, self.f)
+            self.system.assemble(self.A, self.f, self.solution, nodal_loads = nodal_loads)
+            # apply the Dirichlet BCs
+            self.apply_static_condensation(Neumann_dofs)
+            # not enough fixity in the system
+            if (np.linalg.det(self.A) == 0.0):
+                sys.exit("\nSystem not fixed properly.")
+            # check if loads are applied on the system
+            # include a check for elemental loads later!!!
+            elif (np.linalg.norm(nodal_loads, ord=2) == 0.0):
+                sys.exit("\nSystem not loaded.")
+            elif (i == 0):
+                print("\nNewton-Raphson iterations started!!!")
             # solve the linear system
-
-            # assess convergence
+            solution_increment = np.linalg.solve(self.A, self.f)
+            # update the overall solution vector
+            self.solution[Neumann_dofs] += solution_increment
+            # assess convergence (add elemental loads later!!!)
+            # the current external forces
+            f_ext_elemental = np.zeros([self.system.nequations, 1])
+            f_ext = nodal_loads + f_ext_elemental
+            # the current residual (= f_ext - f_int)
+            updated_residual = np.zeros([self.system.nequations, 1])
+            self.system.assemble_residual(updated_residual, self.solution, nodal_loads, element_loads_info = None)
+            # the relative residual norm at all the NEUMANN BOUNDARIES
+            rel_L2_norm = np.linalg.norm(updated_residual[Neumann_dofs], ord=2)/np.linalg.norm(f_ext[Neumann_dofs], ord=2)
+            print("\nIteration:",i+1,",","Relative L2-norm = ",rel_L2_norm)
+            # reset the linear system
+            self.reset_system()
+        print(self.solution)
