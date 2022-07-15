a33517619d6369dd67fb97dd5ce805a6eaea057a
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-07-15 17:18:00 +0200

    Added the bending moment of inertia input

--- lib/sgi/Material.py
+++ lib/sgi/Material.py
@@ -1,8 +1,11 @@
 
 class Material:
     
-    def __init__(self, E, A):
+    def __init__(self, E, A, I):
         # the elastic modulus
         self.E = E
         # the area of the beam cross section
         self.A = A
+        # the area moment of inertia
+        self.I = I
+        print("\nCreated the material.")
