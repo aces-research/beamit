b23aa65f82d62be39d2f26ecdc11da5aa7a13dae
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-07-15 17:18:30 +0200

    Modifications to match the library implementation

--- tests/beamCG/main.py
+++ tests/beamCG/main.py
@@ -1,9 +1,9 @@
-from sgi import FunctionSpace
-from sgi import Material
-from sgi import System
-from sgi import Assembler
-from sgi import Solver
-
+from lib.sgi import FunctionSpace
+from lib.sgi import Material
+from lib.sgi import System
+from lib.sgi import WeakForm
+from lib.sgi import Solver
+import numpy as np
 
 def main():
 
@@ -11,29 +11,31 @@ def main():
     E = 1
     # area of cross section
     A = 1
+    # area moment of inertia
+    I = 1
     # physical information (material parameters)
-    material = Material.Material(E, A)
-    print("Created material")
+    material = Material.Material(E, A, I)
 
     # length of beam
     L = 1
     # number of elements
-    Nel = 10
-    # geometric information (domain, elements, quadrature rule, shape functions)
-    function_space = FunctionSpace.FunctionSpace(0, L, Nel)
+    Nel = 1
+    # geometric information (domain, no. of elements)
+    function_space = FunctionSpace.FunctionSpace(0, L, Nel, discretization_type = "CG")
     function_space.discretize()
-    print("Discretized function space")
 
-    # system as a 
+    # a system binding the function_space (math) and the material (physics) 
     system = System.System(function_space, material)
 
-    # the assembler (in charge of the continuous Galerkin stiffness and residual)
-    assembler = Assembler.AssemblerCG(system)
-
     # the solver
-    solver = Solver.NewtonSolver(assembler)
+    solver = Solver.NewtonRaphsonSolver(system)
+    # apply the boundary conditions
+    # all DOFs fixed on the left most node
+    solver.bctypes[0:1, 0:6] = 0
+    # constant unit force applied on the right most node
+    solver.bcvalues[1:2, 0:1] = 1.0
     # solve the nonlinear static problem and update the nodal position in system 
-    solver.solve()
+    solver.solve(10)
 
 # run main function
 main()
