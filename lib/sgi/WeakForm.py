701daa422aeaa1b7fc22fed72c961b1d2c839156
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-07-16 22:58:27 +0200

    Minor edits and a problem
    Added a few brackets to make the formulation clear and found a potential problem location.

--- lib/sgi/WeakForm.py
+++ lib/sgi/WeakForm.py
@@ -18,7 +18,8 @@ class WeakFormCG:
         rp_dot_rpp = np.sum(rp*rpp, axis=1, keepdims=True)
         rpp_dot_rpp = np.sum(rpp*rpp, axis=1, keepdims=True)
         t1 = (rp*(rp_L2 - 1.0))/rp_L2
-        t2 = 2.0*rp*((rp_dot_rpp**2.0)/(rp_L2**6.0)) - (rp*rpp_dot_rpp)/(rp_L2**4.0) - (rpp*rp_dot_rpp)/(rp_L2**4.0)
+        # A SIGN PROBLEM HERE!!!
+        t2 = (2.0*rp*((rp_dot_rpp**2.0)/(rp_L2**6.0))) - ((rp*rpp_dot_rpp)/(rp_L2**4.0)) - ((rpp*rp_dot_rpp)/(rp_L2**4.0))
         t3 = (rpp/(rp_L2**2.0)) - (rp*rp_dot_rpp)/(rp_L2**4.0)
         axial_integrand = self.material.E*self.material.A*np.matmul(np.transpose(Np, axes=(0, 2, 1)), t1)
         bending_integrand_1 = self.material.E*self.material.I*np.matmul(np.transpose(Np, axes=(0, 2, 1)), t2)
@@ -41,17 +42,17 @@ class WeakFormCG:
         rpp_dyd_rpp = np.matmul(rpp, np.transpose(rpp, axes=(0, 2, 1)))
         imat = np.eye(self.function_space.dim)
         dt1dd = np.matmul(((((rp_L2 - 1.0)/rp_L2)*imat) + ((1.0/(rp_L2**3.0))*rp_dyd_rp)), Np)
-        dt2dd_term1 = (2.0*((rp_dot_rpp**2.0)/(rp_L2**6.0)) - (rpp_dot_rpp)/(rp_L2**4.0))*imat
-        dt2dd_term2 = (-12.0*((rp_dot_rpp**2.0)/(rp_L2**8.0)) + 4.0*((rpp_dot_rpp)/(rp_L2**6.0)))*rp_dyd_rp
+        dt2dd_term1 = ((2.0*((rp_dot_rpp**2.0)/(rp_L2**6.0))) - ((rpp_dot_rpp)/(rp_L2**4.0)))*imat
+        dt2dd_term2 = ((-12.0*((rp_dot_rpp**2.0)/(rp_L2**8.0))) + (4.0*((rpp_dot_rpp)/(rp_L2**6.0))))*rp_dyd_rp
         dt2dd_term3 = (4.0*((rp_dot_rpp)/(rp_L2**6.0)))*(rp_dyd_rpp + rpp_dyd_rp)
         dt2dd_term4 = (1.0/(rp_L2**4.0))*rpp_dyd_rpp
-        dt2dd_term5 = -((rp_dot_rpp)/(rp_L2**4.0))*imat + (4.0*((rp_dot_rpp)/(rp_L2**6.0)))*(rp_dyd_rp)
-        dt2dd_term6 = (2.0/(rp_L2**4.0))*rp_dyd_rpp + (1.0/(rp_L2**4.0))*rpp_dyd_rp
+        dt2dd_term5 = -(((rp_dot_rpp)/(rp_L2**4.0))*imat) + ((4.0*((rp_dot_rpp)/(rp_L2**6.0)))*rp_dyd_rp)
+        dt2dd_term6 = ((2.0/(rp_L2**4.0))*rp_dyd_rpp) + ((1.0/(rp_L2**4.0))*rpp_dyd_rp)
         dt2dd = np.matmul((dt2dd_term1 + dt2dd_term2 + dt2dd_term3 - dt2dd_term4), Np) + \
                                     np.matmul((dt2dd_term5 - dt2dd_term6), Npp)
         dt3dd_term1 = dt2dd_term5
-        dt3dd_term2 = (2.0/(rp_L2**4.0))*rpp_dyd_rp + (1.0/(rp_L2**4.0))*rp_dyd_rpp
-        dt3dd_term3 = (1.0/(rp_L2**2.0))*imat - (1.0/(rp_L2**4.0))*(rp_dyd_rp) 
+        dt3dd_term2 = ((2.0/(rp_L2**4.0))*rpp_dyd_rp) + ((1.0/(rp_L2**4.0))*rp_dyd_rpp)
+        dt3dd_term3 = ((1.0/(rp_L2**2.0))*imat) - ((1.0/(rp_L2**4.0))*rp_dyd_rp) 
         dt3dd = np.matmul((dt3dd_term1 - dt3dd_term2), Np) + np.matmul((dt3dd_term3), Npp)
         dt4dd = np.matmul(((1.0/(rp_L2**2.0))*imat - (2.0/(rp_L2**4.0))*(rp_dyd_rp)), Np)
         axial_integrand = self.material.E*self.material.A*np.matmul(np.transpose(Np, axes=(0, 2, 1)), dt1dd)
