a145992bdb1c0a15e8a3f1902f822f65c33f0ec4
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-07-16 23:03:09 +0200

    Checks, residual and tolerance
    Added some more checks, modified the residual and added tolerance to the solver.

--- lib/sgi/Solver.py
+++ lib/sgi/Solver.py
@@ -46,16 +46,16 @@ class NewtonRaphsonSolver:
         self.A = self.A[np.ix_(Neumann_dofs, Neumann_dofs)]
         self.f = self.f[Neumann_dofs]
     
-    def solve(self, Nmax = 10):
+    def solve(self, Nmax = 10, tol = 1.0E-05):
         # create the Dirichlet and Neumann global dof arrays
         Dirichlet_dofs, Neumann_dofs = self.create_dof_arrays()
         # If Dirichlet boundary conditions are not available
         if (Dirichlet_dofs.size == 0):
-            sys.exit("\nDirichlet boundary conditions not found.")
+            sys.exit("\nDirichlet boundary conditions are not found.")
         else:
             # generate the nodal load vector
             nodal_loads = np.zeros([self.system.nequations, 1])
-            nodal_loads[Neumann_dofs] = np.reshape(self.bcvalues, [self.system.nequations, 1])[Neumann_dofs]
+            nodal_loads[Neumann_dofs] += np.reshape(self.bcvalues, [self.system.nequations, 1])[Neumann_dofs]
             # generate the Dirichlet solution vector
             Dirichlet_solution = np.reshape(self.bcvalues, [self.system.nequations, 1])[Dirichlet_dofs]
             # add the Dirichlet solution to the overall solution vector
@@ -66,29 +66,38 @@ class NewtonRaphsonSolver:
             self.system.assemble(self.A, self.f, self.solution, nodal_loads = nodal_loads)
             # apply the Dirichlet BCs
             self.apply_static_condensation(Neumann_dofs)
-            # not enough fixity in the system
-            if (np.linalg.det(self.A) == 0.0):
-                sys.exit("\nSystem not fixed properly.")
-            # check if loads are applied on the system
-            # include a check for elemental loads later!!!
-            elif (np.linalg.norm(nodal_loads, ord=2) == 0.0):
-                sys.exit("\nSystem not loaded.")
-            elif (i == 0):
-                print("\nNewton-Raphson iterations started!!!")
+            # checks in the first iteration
+            if (i == 0):
+                # not enough fixity in the system
+                if (np.linalg.det(self.A) == 0.0):
+                    sys.exit("\nSystem is not fixed properly.")
+                # if loads are applied on the system
+                # include a check for elemental loads later!!!
+                elif (np.linalg.norm(nodal_loads, ord=2) == 0.0):
+                    sys.exit("\nSystem is not loaded.")
+                else:
+                    print("\nStarting the Newton-Raphson iterations!!!")
+            # checks after the first iteration
+            else:
+                # instability in the system
+                if (np.linalg.det(self.A) == 0.0):
+                    sys.exit("\nSystem is unstable.")
             # solve the linear system
             solution_increment = np.linalg.solve(self.A, self.f)
             # update the overall solution vector
             self.solution[Neumann_dofs] += solution_increment
-            # assess convergence (add elemental loads later!!!)
-            # the current external forces
-            f_ext_elemental = np.zeros([self.system.nequations, 1])
-            f_ext = nodal_loads + f_ext_elemental
+            # assess convergence
             # the current residual (= f_ext - f_int)
             updated_residual = np.zeros([self.system.nequations, 1])
             self.system.assemble_residual(updated_residual, self.solution, nodal_loads, element_loads_info = None)
-            # the relative residual norm at all the NEUMANN BOUNDARIES
-            rel_L2_norm = np.linalg.norm(updated_residual[Neumann_dofs], ord=2)/np.linalg.norm(f_ext[Neumann_dofs], ord=2)
-            print("\nIteration:",i+1,",","Relative L2-norm = ",rel_L2_norm)
-            # reset the linear system
-            self.reset_system()
-        print(self.solution)
+            # the residual norm at all the NEUMANN NODES
+            res_L2_norm = np.linalg.norm(updated_residual[Neumann_dofs], ord=2)
+            print("\nIteration:",i+1,", Residual L2-norm = %.2e" % (res_L2_norm))
+            if (res_L2_norm <= tol):
+                print("\nSolver converged!!!")
+                break
+            else:
+                # reset the linear system
+                self.reset_system()
+        # update the system attributes
+        self.system.update(self.solution)
