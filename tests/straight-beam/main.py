41010e677bc09fdbfd4e4d9605eb46ea1d56479c
Bianca Giovanardi <b.giovanardi@tudelft.nl>
2022-07-18 18:45:32 +0200

    tests/straight-beam: add another test on straight beam
    Tangents are aligned with x-axis, apply no forces, assert that the beam stays straight and aligned with the x-axis.

--- tests/straight-beam/main.py
+++ tests/straight-beam/main.py
@@ -4,8 +4,9 @@ from beamit import System
 from beamit import Solver
 import numpy as np
 
-def test():
-    
+
+def test_clamp():
+
     # elastic modulus of beam
     E = 1
     # area of cross section
@@ -43,7 +44,8 @@ def test():
         np.array([0.,0.,0.,1.,0.,0.,1.,0.,0.,1.,0.,0.])
         ==0))
 
-def test():
+
+def test_fix_position():
 
     # elastic modulus of beam
     E = 1
@@ -74,6 +76,47 @@ def test():
     solver.bcvalues[0, 0:3] = 0
     solver.bctypes[1, 0:3] = 1
     solver.bcvalues[1, 0:3] = 0
+
+    # solve the nonlinear static problem and update the nodal position in system 
+    solver.solve(1, 1.0E-05)
+
+    # assert that the solution is a straight beam aligned with the x-axis
+    assert(np.all(np.transpose(solver.solution) -
+        np.array([0.,0.,0.,1.,0.,0.,1.,0.,0.,1.,0.,0.])
+        ==0))
+
+
+def test_fix_tangent():
+
+    # elastic modulus of beam
+    E = 1
+    # area of cross section
+    A = 1
+    # area moment of inertia
+    I = 1
+    # physical information (material parameters)
+    material = Material.Material(E, A, I)
+
+    # length of beam
+    L = 1
+    # number of elements
+    Nel = 1
+    # geometric information (domain, no. of elements)
+    function_space = FunctionSpace.FunctionSpace(0, L, Nel, discretization_type = "CG")
+    function_space.discretize()
+
+    # a system binding the function_space (math) and the material (physics) 
+    system = System.System(function_space, material)
+
+    # the solver
+    solver = Solver.NewtonRaphsonSolver(system)
+
+    # apply the boundary conditions
+    # all the tangents are [1,0,0] 
+    solver.bctypes[0, 3:6] = 1
+    solver.bcvalues[0, 3] = 1
+    solver.bctypes[1, 3:6] = 1
+    solver.bcvalues[1, 3] = 1
 
     # solve the nonlinear static problem and update the nodal position in system 
     solver.solve(1, 1.0E-05)
