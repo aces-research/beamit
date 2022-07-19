05f0db963cf194370e831129fa639c78abf6959c
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-07-19 14:26:15 +0200

    Added system state and an edit in the nodal forces
    Modified the nodal_coordinates to state & the update function. A correction related to computing the nodal contributions with the moments.

--- lib/beamit/System.py
+++ lib/beamit/System.py
@@ -11,17 +11,36 @@ class System:
         else:
             sys.exit("\nWeak form of the discretization is not available.")
         # the initial state of the system
-        self.nodal_coordinates = self.weak_form.function_space.nodes
+        self.state = self.initialize_state()
         # the number of equations
         self.nequations = self.weak_form.function_space.N*self.weak_form.function_space.dof
+
+    # Function to initialize the state of the system
+    def initialize_state(self):
+        initial_state = np.zeros([self.weak_form.function_space.N, self.weak_form.function_space.dof])
+        # set the initial positions as the nodal co-ordinates
+        initial_state[:, 0:3] = self.weak_form.function_space.nodes
+        # assuming initially straight beams are along the x-axis!!!
+        initial_state[:, 3:4] = 1.0
+        return initial_state
 
     def assemble_stiffness(self, A, solution):
         self.weak_form.compute_system_stiffness(A, solution)
         pass
 
     def assemble_residual(self, f, solution, nodal_loads, element_loads_info):
+        dofs = self.weak_form.function_space.dof
         self.weak_form.compute_system_residual(f, solution, element_loads_info)
-        f += nodal_loads
+        updated_nodal_loads = np.zeros(nodal_loads.shape)
+        for i in range(0, self.weak_form.function_space.N):
+            updated_nodal_loads[dofs*i:(dofs*i)+3, :] += nodal_loads[dofs*i:(dofs*i)+3, :]
+            nodal_tangents = solution[(dofs*i)+3:(dofs*i)+6, :]
+            nodal_tangents_L2 = np.linalg.norm(nodal_tangents, ord=2, axis=0, keepdims=True)
+            t4_nodal = nodal_tangents/(nodal_tangents_L2**2.0)
+            # compute the cross-product for the nodal moments
+            updated_nodal_loads[(dofs*i)+3:(dofs*i)+6, :] += \
+                    np.cross(nodal_loads[(dofs*i)+3:(dofs*i)+6, :], t4_nodal, axisa=0, axisb=0, axisc=0)
+        f += updated_nodal_loads
         pass
 
     def assemble(self, A, f, solution, nodal_loads = 0.0, element_loads_info = None):
@@ -30,17 +49,9 @@ class System:
 
         # assemble residual
         self.assemble_residual(f, solution, nodal_loads, element_loads_info)
-    
-    # Function to update the node positions (nodal co-ordinates)
-    def update_node_positions(self, solution):
-        solution_matrix = np.reshape(solution, [self.weak_form.function_space.N, \
-                                self.weak_form.function_space.dof])
-        # first three columns in the solution matrix are the new positions
-        self.nodal_coordinates = solution_matrix[:, 0:3]
-        pass
 
-    # Function to update the attributes of the system
+    # Function to update the state of the system
     def update(self, solution):
-        # update the node positions
-        self.update_node_positions(solution)
+        self.state = np.reshape(solution, [self.weak_form.function_space.N, \
+                                self.weak_form.function_space.dof])
         pass
