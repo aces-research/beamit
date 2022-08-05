f678090970e76be9ca311e2f0a9539ce5ec69a86
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-08-05 16:57:49 +0200

    Added load cases and some minor modifications

--- tests/beamCG/main.py
+++ tests/beamCG/main.py
@@ -11,11 +11,11 @@ def static_main():
     # density of the material
     rho = 7850.0
     # elastic modulus of beam
-    E = 2.0e11
+    E = 2.0E11
     # area of cross section
     A = 0.0314159
     # area moment of inertia
-    I = 7.85398e-5
+    I = 7.85398E-05
     # physical information (material parameters)
     material = Material.Material(rho, E, A, I)
 
@@ -84,11 +84,11 @@ def dynamic_main():
     # density of the material
     rho = 7850.0
     # elastic modulus of beam
-    E = 2.0e11
+    E = 2.0E11
     # area of cross section
     A = 0.0314159
     # area moment of inertia
-    I = 7.85398e-5
+    I = 7.85398E-05
     # physical information (material parameters)
     material = Material.Material(rho, E, A, I)
 
@@ -103,6 +103,7 @@ def dynamic_main():
 
     # a system binding the function_space (math) and the material (physics) 
     system = System.System(function_space, material)
+    initial_state = system.state
 
     # the solver
     solver = Solver.NewmarkSolver(system)
@@ -112,28 +113,68 @@ def dynamic_main():
     # boundary condition values matrix
     bcvalues = np.zeros([function_space.N, function_space.dof])
 
+    # applied loads and tolerances
+    FORCE_Y = -1.0E04
+    FORCE_Z = -1.0E04
+    MOMENT_Z = 1.0E07
+    FORCE_X_CB = -5.0E05
+    PERTURB_CB = 100.0
+    SPATIAL_TOLERANCE = 1.0E-05
+      
     # function to apply BCs
-    def get_supports(bctypes, bcvalues, load_case):
-        if ((load_case == 0) or (load_case == 1)):
-            for i in range(0, nodal_coordinates.shape[0]): # loop over the nodes
-                x_coord = nodal_coordinates[i, 0]
-                y_coord = nodal_coordinates[i, 1]
-                z_coord = nodal_coordinates[i, 2]
-                # clamp left node (a clamp fixes both the position and the direction of the tangent)
-                if ((x_coord == 0.0) and (y_coord == 0.0) and (z_coord == 0.0)):
+    def update_BCs(bctypes, bcvalues, load_case, load_level):
+        for i in range(0, nodal_coordinates.shape[0]): # loop over the nodes
+            x_coord = nodal_coordinates[i, 0]
+            y_coord = nodal_coordinates[i, 1]
+            z_coord = nodal_coordinates[i, 2]
+            if (load_case == 0):
+                # clamp left node
+                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
                     bctypes[i, 0:3] = 1
                     bctypes[i, 4:6] = 1
-                    bcvalues[i, 0:3] = system.state[i, 0:3]
-                    bcvalues[i, 4:6] = system.state[i, 4:6]
-
-    # applied loads
-    force_x = 0.0
-    force_y = 0.0
-    force_z = -1.0e4
-    moment_y = 1.0e7
+                    bcvalues[i, 0:3] = initial_state[i, 0:3]
+                    bcvalues[i, 4:6] = initial_state[i, 4:6]
+                # apply force on right node
+                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bcvalues[i, 2] = load_level*FORCE_Z
+            elif (load_case == 1):
+                # clamp left node
+                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bctypes[i, 0:3] = 1
+                    bctypes[i, 4:6] = 1
+                    bcvalues[i, 0:3] = initial_state[i, 0:3]
+                    bcvalues[i, 4:6] = initial_state[i, 4:6]
+                # apply moment on right node
+                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bcvalues[i, 5] = load_level*MOMENT_Z
+            elif(load_case == 3):
+                # pin at left end
+                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bctypes[i, 0:3] = 1
+                    bcvalues[i, 0:3] = initial_state[i, 0:3]
+                # roller and axial load at the right end
+                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bctypes[i, 1:3] = 1
+                    bcvalues[i, 1:3] = initial_state[i, 1:3]
+                    bcvalues[i, 0] = load_level*FORCE_X_CB
+                # perturbation force at the center
+                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bcvalues[i, 1] = PERTURB_CB
+            elif(load_case == 14):
+                # pin at left end
+                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bctypes[i, 0:3] = 1
+                    bcvalues[i, 0:3] = initial_state[i, 0:3]
+                # roller at the right end
+                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bctypes[i, 1:3] = 1
+                    bcvalues[i, 1:3] = initial_state[i, 1:3]
+                # apply force at the center
+                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
+                    bcvalues[i, 1] = load_level*FORCE_Y
 
     # the load case
-    load_case = 1
+    load_case = 3
 
     # create a VTK directory or clear it
     if not os.path.isdir("VTK"):
@@ -142,31 +183,23 @@ def dynamic_main():
         for item in os.listdir("VTK"):
             os.remove(os.path.join("VTK", item))
     
-    # write the initial positions
+    # write the initial displacements
     output_file = "./VTK/output-0"
-    PostProcess.write_positions_vtk(output_file, system)
+    PostProcess.write_displacements_vtk(output_file, system)
     
     # solve the nonlinear dynamic problem and update the nodal position in system 
     time_steps = 10000 # No. of time steps
     save_time = 100
-    get_supports(bctypes, bcvalues, load_case)
     for i in range(0, time_steps):
         print("\nCurrent time step:", i+1,"out of", time_steps, "time steps.")
         output_file = "./VTK/output-" + str(i+1)
-        # apply loads
-        if (load_case == 0):
-            # apply constant force on right most node
-            bcvalues[Nel, 0] = force_x*((i+1)/time_steps)
-            bcvalues[Nel, 1] = force_y*((i+1)/time_steps)
-            bcvalues[Nel, 2] = force_z*((i+1)/time_steps)
-        elif (load_case == 1):
-            # apply constant moment on right most node
-            bcvalues[Nel, 4] = moment_y*((i+1)/time_steps)
+        load_level = (i+1)/time_steps
+        update_BCs(bctypes, bcvalues, load_case, load_level)
         # apply the boundary conditions
         solver.set_boundary_conditions(bctypes, bcvalues)
-        solver.solve(dt = 0.01, Nmax = 50)
+        solver.solve(dt = 0.01, Nmax = 20, tol = 1.0E-03)
         if ((i+1) % save_time == 0):
-            PostProcess.write_positions_vtk(output_file, system)
+            PostProcess.write_displacements_vtk(output_file, system)
 
 # run main functions
 # static_main()
