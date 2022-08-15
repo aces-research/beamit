c36ab0e918476ba8aa6a885fc321bd04f8dd85f1
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2022-08-15 10:20:01 +0200

    Added DG weak form code and residual for CG
    Added condition to invoke the DG weak form and the assemble residual function is made specific to the CG discretization.

--- lib/beamit/System.py
+++ lib/beamit/System.py
@@ -8,6 +8,9 @@ class System:
         if (function_space.discretization_type == "CG"):
             # the continuous Galerkin weak form
             self.weak_form = WeakForm.WeakFormCG(function_space, material)
+        elif (function_space.discretization_type == "DG"):
+            # the discontinuous Galerkin weak form
+            self.weak_form = WeakForm.WeakFormDG(function_space, material)
         else:
             sys.exit("\nWeak form of the discretization is not available.")
         # the initial state of the system
@@ -29,18 +32,19 @@ class System:
         pass
 
     def assemble_residual(self, f, solution, nodal_loads, element_loads_info):
-        dofs = self.weak_form.function_space.dof
-        self.weak_form.compute_system_residual(f, solution, element_loads_info)
-        updated_nodal_loads = np.zeros(nodal_loads.shape)
-        for i in range(0, self.weak_form.function_space.N):
-            updated_nodal_loads[dofs*i:(dofs*i)+3, :] += nodal_loads[dofs*i:(dofs*i)+3, :]
-            nodal_tangents = solution[(dofs*i)+3:(dofs*i)+6, :]
-            nodal_tangents_L2 = np.linalg.norm(nodal_tangents, ord=2, axis=0, keepdims=True)
-            t4_nodal = nodal_tangents/(nodal_tangents_L2**2.0)
-            # compute the cross-product for the nodal moments
-            updated_nodal_loads[(dofs*i)+3:(dofs*i)+6, :] += \
-                    np.cross(nodal_loads[(dofs*i)+3:(dofs*i)+6, :], t4_nodal, axisa=0, axisb=0, axisc=0)
-        f += updated_nodal_loads
+        if (self.weak_form.function_space.discretization_type == "CG"):
+            dofs = self.weak_form.function_space.dof
+            self.weak_form.compute_system_residual(f, solution, element_loads_info)
+            updated_nodal_loads = np.zeros(nodal_loads.shape)
+            for i in range(0, self.weak_form.function_space.N):
+                updated_nodal_loads[dofs*i:(dofs*i)+3, :] += nodal_loads[dofs*i:(dofs*i)+3, :]
+                nodal_tangents = solution[(dofs*i)+3:(dofs*i)+6, :]
+                nodal_tangents_L2 = np.linalg.norm(nodal_tangents, ord=2, axis=0, keepdims=True)
+                t4_nodal = nodal_tangents/(nodal_tangents_L2**2.0)
+                # compute the cross-product for the nodal moments
+                updated_nodal_loads[(dofs*i)+3:(dofs*i)+6, :] += \
+                        np.cross(nodal_loads[(dofs*i)+3:(dofs*i)+6, :], t4_nodal, axisa=0, axisb=0, axisc=0)
+            f += updated_nodal_loads
         pass
 
     def assemble_mass(self, M):
