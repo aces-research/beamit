eb2afb040cf3c6eed096ca39d931cb1cb5ef5370
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2023-03-21 09:14:17 +0100

    System frequencies based on the initial stiffness
    Computing the system frequencies based on the initial stiffness of the beam. There could be problems in the evaluation of system frequencies based on the beam's deformed state because fracture and other non-linearities can lead to complex Eigenvalues.

--- lib/beamit/Solver.py
+++ lib/beamit/Solver.py
@@ -303,14 +303,13 @@ class ExplicitNewmarkSolver(DynamicSolver):
         if (Dirichlet_dofs.size == 0):
             sys.exit("\nDirichlet boundary conditions are not found.")
         stiffness = np.zeros([self.system.nequations, self.system.nequations])
-        residual = np.zeros([self.system.nequations, 1])
-        self.system.assemble(stiffness, residual, self.solution, nodal_loads = np.zeros([self.system.nequations, 1]))
+        # compute the initial stiffness of the system
+        self.system.weak_form.compute_system_initial_stiffness(stiffness)
         eig_vals, eig_vecs = sp.linalg.eig(stiffness[np.ix_(Neumann_dofs, Neumann_dofs)], self.M[np.ix_(Neumann_dofs, Neumann_dofs)])
         mode_shapes = np.zeros([self.system.nequations, self.system.nequations-Dirichlet_dofs.size])
         if (np.linalg.norm(eig_vals.imag) != 0.0):
             sys.exit("\nThe Eigenvalues of the system are complex valued.")
         mode_shapes[Neumann_dofs, :] += eig_vecs
-        mode_shapes[Dirichlet_dofs, :] += self.solution[Dirichlet_dofs]*np.ones([Dirichlet_dofs.size, self.system.nequations-Dirichlet_dofs.size])
         return np.sqrt(eig_vals), mode_shapes
     
     # Function to compute and set the stable time step
