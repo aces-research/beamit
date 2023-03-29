58ac809e00ea041745d43c80f787b6748a74bb40
Sai Kubair Kota <S.K.Kota@tudelft.nl>
2023-03-29 14:01:37 +0200

    Damping matrix and system reset function
    Initialized the damping matrix, and added a function to reset the mass and damping matrices along with the stiffness matrix and the residual.

--- lib/beamit/Solver.py
+++ lib/beamit/Solver.py
@@ -170,14 +170,21 @@ class DynamicSolver(Solver):
     def __init__(self, system):
         # invoke the parent (Solver) class
         Solver.__init__(self, system)
-        # initialize and assemble the mass matrix
+        # initialize the mass and damping matrices
         self.M = np.zeros([system.nequations, system.nequations])
-        self.system.assemble_mass(self.M)
+        self.C = np.zeros([system.nequations, system.nequations])
         # the "velocity" (linear velocities and time derivative of the tangents)
         self.velocity = np.zeros([system.nequations, 1])
         # the "acceleration" (linear accelerations and double time derivative of the tangents)
         self.acceleration = np.zeros([system.nequations, 1])
 
+    # Function to reset the dynamic linear system
+    def reset_system(self):
+        super().reset_system()
+        self.M = np.zeros([self.system.nequations, self.system.nequations])
+        self.C = np.zeros([self.system.nequations, self.system.nequations])
+        pass
+    
     # Function to set the initial conditions (position and velocity) of the system
     def set_initial_conditions(self, initial_position, initial_velocity):
         self.solution = copy.deepcopy(np.reshape(initial_position, [self.system.nequations, 1]))
