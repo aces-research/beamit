b619bbb76e6f2a0874f0b95dc30483e77f5be57a
Sai Kubair Kota <s.k.kota@tudelft.nl>
2023-11-28 10:06:43 +0100

    single_element_tests: EB beam stiffness & residual
    Added a single element tests for checking the Euler-Bernoulli beam stiffness and residual against the exact quantities.

---
+++ tests/euler-bernoulli/single_element_tests.py
@@ -0,0 +1,54 @@
+import numpy as np
+from beamit import FunctionSpace, Material
+from beamit import Material
+from beamit import System
+
+# density of the material
+rho = 1000.0
+# elastic modulus of beam
+E = 1.0E12
+# the radius of the beam
+R = 1.0E-03
+# length of beam
+L = 0.1
+# number of elements
+Nel = 1
+# tolerance
+NUMERICAL_TOLERANCE = 1.0E-10
+
+if __name__ == "__main__":
+
+    # physical information (material parameters)
+    material = Material.Material(rho, E, R=R)
+
+    # geometric information (domain, no. of elements)
+    function_space = FunctionSpace.EulerBernoulliFunctionSpace(0.0, L, Nel, discretization_type = "CG")
+    function_space.discretize()
+
+    # a system binding the function_space (math) and the material (physics) 
+    system = System.System(function_space, material)
+
+    # test the element stiffness matrix
+    computed_stiffness_matrix = \
+        system.weak_form.compute_element_internal_stiffness(np.zeros([function_space.npel*function_space.dof, 1]))
+    actual_stiffness_matrix = np.array([[((12.0*E*material.I)/L**3.0), ((6.0*E*material.I)/L**2.0), \
+                                       -((12.0*E*material.I)/L**3.0), ((6.0*E*material.I)/L**2.0)], \
+                                        [((6.0*E*material.I)/L**2.0), ((4.0*E*material.I)/L), \
+                                         -((6.0*E*material.I)/L**2.0), ((2.0*E*material.I)/L)], \
+                                        [-((12.0*E*material.I)/L**3.0), -((6.0*E*material.I)/L**2.0), \
+                                         ((12.0*E*material.I)/L**3.0), -((6.0*E*material.I)/L**2.0)], \
+                                        [((6.0*E*material.I)/L**2.0), ((2.0*E*material.I)/L), \
+                                         -((6.0*E*material.I)/L**2.0), ((4.0*E*material.I)/L)]])
+
+    assert np.linalg.norm(computed_stiffness_matrix - actual_stiffness_matrix) < NUMERICAL_TOLERANCE, \
+            f"Euler-Bernoulli beam stiffness matrix test failed."
+    print("\nEuler-Bernoulli beam stiffness matrix test passed!")
+
+    # test the internal element residual
+    random_solution = np.random.rand(function_space.npel*function_space.dof, 1)
+    computed_internal_residual = \
+        system.weak_form.compute_element_internal_forces(random_solution)
+    assert np.linalg.norm(computed_internal_residual - \
+                          np.matmul(actual_stiffness_matrix, random_solution)) < NUMERICAL_TOLERANCE, \
+            f"Euler-Bernoulli beam internal residual test failed."
+    print("\nEuler-Bernoulli beam internal residual test passed!")    
