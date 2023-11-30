c716880b69f844edf5cce883d886be536b293718
Sai Kubair Kota <s.k.kota@tudelft.nl>
2023-11-30 13:21:18 +0100

    EBWeakFormDG: added Euler-Bernoulli DG weak form
    Added the Euler-Bernoulli DG weak form class which implements methods related to the DG discretization of the Euler-Bernoulli beam formulation.

--- lib/beamit/WeakForm.py
+++ lib/beamit/WeakForm.py
@@ -772,3 +772,207 @@ class EulerBernoulliWeakFormCG(WeakFormCG):
                 f[global_element_dofs_left_node[1:2]] += shear_force_left_node
                 f[global_element_dofs_left_node[2:3]] += bending_moment_left_node
         pass
+
+class EulerBernoulliWeakFormDG(EulerBernoulliWeakFormCG):
+    
+    def __init__(self, function_space, material, beta):
+        # invoke the parent (EulerBernoulliWeakFormCG) class
+        EulerBernoulliWeakFormCG.__init__(self, function_space, material)
+        # DG penalty parameter
+        self.beta = beta
+        # shape function values at the interface
+        phi_left_interface, phixi_left_interface = self.function_space.compute_lagrange_shapes(1.0)
+        N_left_interface, Nxi_left_interface, Nxixi_left_interface, Nxixixi_left_interface = \
+                                    self.function_space.compute_hermite_shapes(1.0)
+        phi_right_interface, phixi_right_interface = self.function_space.compute_lagrange_shapes(-1.0)
+        N_right_interface, Nxi_right_interface, Nxixi_right_interface, Nxixixi_right_interface = \
+                                    self.function_space.compute_hermite_shapes(-1.0)
+        self.phi_left_interface = phi_left_interface
+        self.phix_left_interface = phixi_left_interface*(1.0/self.function_space.jacobian)
+        self.N_left_interface = N_left_interface
+        self.Nx_left_interface = Nxi_left_interface*(1.0/self.function_space.jacobian)
+        self.Nxx_left_interface = Nxixi_left_interface*((1.0/self.function_space.jacobian)**2.0)
+        self.Nxxx_left_interface = Nxixixi_left_interface*((1.0/self.function_space.jacobian)**3.0)
+        self.phi_right_interface = phi_right_interface
+        self.phix_right_interface = phixi_right_interface*(1.0/self.function_space.jacobian)
+        self.N_right_interface = N_right_interface
+        self.Nx_right_interface = Nxi_right_interface*(1.0/self.function_space.jacobian)
+        self.Nxx_right_interface = Nxixi_right_interface*((1.0/self.function_space.jacobian)**2.0)
+        self.Nxxx_right_interface = Nxixixi_right_interface*((1.0/self.function_space.jacobian)**3.0)
+
+    # Function to compute interface forces
+    def compute_interface_forces(self, element_unknowns_left, element_unknowns_right):
+        # dofs and their derivaitives at the interface
+        # left side
+        u_left = np.matmul(self.phi_left_interface, element_unknowns_left)
+        axial_force_left = self.material.E*self.material.A * \
+                    np.matmul(self.phix_left_interface, element_unknowns_left)
+        w_left = np.matmul(self.N_left_interface, element_unknowns_left)
+        wx_left = np.matmul(self.Nx_left_interface, element_unknowns_left)
+        shear_force_left = -self.material.E*self.material.I * \
+                                np.matmul(self.Nxxx_left_interface, element_unknowns_left)
+        bending_moment_left = -self.material.E*self.material.I * \
+                                np.matmul(self.Nxx_left_interface, element_unknowns_left)
+        # right side
+        u_right = np.matmul(self.phi_right_interface, element_unknowns_right)
+        axial_force_right = self.material.E*self.material.A * \
+                    np.matmul(self.phix_right_interface, element_unknowns_right)
+        w_right = np.matmul(self.N_right_interface, element_unknowns_right)
+        wx_right = np.matmul(self.Nx_right_interface, element_unknowns_right)
+        shear_force_right = -self.material.E*self.material.I * \
+                                np.matmul(self.Nxxx_right_interface, element_unknowns_right)
+        bending_moment_right = -self.material.E*self.material.I * \
+                                np.matmul(self.Nxx_right_interface, element_unknowns_right)
+        # jumps at the interface
+        u_jump = u_right - u_left
+        w_jump = w_right - w_left
+        wx_jump = wx_right - wx_left
+        # "forces" at the interface
+        axial_forces_interface = ((axial_force_left + axial_force_right) / 2.0) + \
+                                        (self.beta*((self.material.E*self.material.A) / \
+                                                   self.function_space.elL)*u_jump)
+        shear_forces_interface = ((shear_force_left + shear_force_right) / 2.0) + \
+                                        (self.beta*((self.material.E*self.material.A) / \
+                                                   self.function_space.elL)*w_jump)
+        bending_moments_interface = -((bending_moment_left + bending_moment_right) / 2.0) + \
+                                        (self.beta*((self.material.E*self.material.I) / \
+                                                   self.function_space.elL)*wx_jump)
+        return axial_forces_interface, shear_forces_interface, bending_moments_interface
+
+    # Function to compute the system residual
+    def compute_system_residual(self, f, system_unknowns, element_loads_info):
+        # compute system residual using the function in EulerBernoulliWeakFormCG
+        super().compute_system_residual(f, system_unknowns, element_loads_info)
+        # loop over the interfaces
+        for i in range(0, self.function_space.E-1):
+            # since the elements are placed one after the other like a simple chain!!!
+            # current element (= left (-)) and next element (= right (+))
+            global_element_dofs_left = self.function_space.global_connectivity[i:i+1].flatten()
+            global_element_dofs_right = self.function_space.global_connectivity[i+1:i+2].flatten()
+            # unknowns of the left and right elements
+            element_unknowns_left = system_unknowns[global_element_dofs_left]
+            element_unknowns_right = system_unknowns[global_element_dofs_right]
+            axial_forces_interface, shear_forces_interface, bending_moments_interface = \
+                self.compute_interface_forces(element_unknowns_left, element_unknowns_right)
+            # assemble the interface forces
+            f[global_element_dofs_left] += \
+                    np.matmul(np.transpose(self.phi_left_interface), axial_forces_interface) + \
+                    np.matmul(np.transpose(self.N_left_interface), shear_forces_interface) + \
+                    np.matmul(np.transpose(self.Nx_left_interface), bending_moments_interface)
+            f[global_element_dofs_right] -= \
+                    np.matmul(np.transpose(self.phi_right_interface), axial_forces_interface) + \
+                    np.matmul(np.transpose(self.N_right_interface), shear_forces_interface) + \
+                    np.matmul(np.transpose(self.Nx_right_interface), bending_moments_interface)
+        pass
+
+    # Function to compute the system stiffness
+    def compute_system_stiffness(self, A, system_unknowns, element_loads_info):
+        # compute system stiffness using the function in EulerBernoulliWeakFormCG
+        super().compute_system_stiffness(A, system_unknowns, element_loads_info)
+        # loop over the interfaces
+        for i in range(0, self.function_space.E-1):
+            # since the elements are placed one after the other like a simple chain!!!
+            # current element (= left (-)) and next element (= right (+))
+            global_element_dofs_left = self.function_space.global_connectivity[i:i+1].flatten()
+            global_element_dofs_right = self.function_space.global_connectivity[i+1:i+2].flatten()
+            # flux term tangents
+            A[np.ix_(global_element_dofs_left, global_element_dofs_left)] -= \
+                (0.50*self.material.E*self.material.A*np.matmul(np.transpose(self.phi_left_interface), \
+                                                          self.phix_left_interface)) - \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.N_left_interface), \
+                                                          self.Nxxx_left_interface)) + \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.Nx_left_interface), \
+                                                          self.Nxx_left_interface))
+            A[np.ix_(global_element_dofs_left, global_element_dofs_right)] -= \
+                (0.50*self.material.E*self.material.A*np.matmul(np.transpose(self.phi_left_interface), \
+                                                          self.phix_right_interface)) - \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.N_left_interface), \
+                                                          self.Nxxx_right_interface)) + \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.Nx_left_interface), \
+                                                          self.Nxx_right_interface))
+            A[np.ix_(global_element_dofs_right, global_element_dofs_left)] += \
+                (0.50*self.material.E*self.material.A*np.matmul(np.transpose(self.phi_right_interface), \
+                                                          self.phix_left_interface)) - \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.N_right_interface), \
+                                                          self.Nxxx_left_interface)) + \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.Nx_right_interface), \
+                                                          self.Nxx_left_interface))
+            A[np.ix_(global_element_dofs_right, global_element_dofs_right)] += \
+                (0.50*self.material.E*self.material.A*np.matmul(np.transpose(self.phi_right_interface), \
+                                                          self.phix_right_interface)) - \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.N_right_interface), \
+                                                          self.Nxxx_right_interface)) + \
+                (0.50*self.material.E*self.material.I*np.matmul(np.transpose(self.Nx_right_interface), \
+                                                          self.Nxx_right_interface))
+            # penalty term tangents
+            A[np.ix_(global_element_dofs_left, global_element_dofs_left)] += \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.phi_left_interface), self.phi_left_interface)) + \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.N_left_interface), self.N_left_interface)) + \
+                (self.beta*((self.material.E*self.material.I)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.Nx_left_interface), self.Nx_left_interface))
+            A[np.ix_(global_element_dofs_left, global_element_dofs_right)] -= \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.phi_left_interface), self.phi_right_interface)) + \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.N_left_interface), self.N_right_interface)) + \
+                (self.beta*((self.material.E*self.material.I)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.Nx_left_interface), self.Nx_right_interface))
+            A[np.ix_(global_element_dofs_right, global_element_dofs_left)] -= \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.phi_right_interface), self.phi_left_interface)) + \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.N_right_interface), self.N_left_interface)) + \
+                (self.beta*((self.material.E*self.material.I)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.Nx_right_interface), self.Nx_left_interface))
+            A[np.ix_(global_element_dofs_right, global_element_dofs_right)] += \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.phi_right_interface), self.phi_right_interface)) + \
+                (self.beta*((self.material.E*self.material.A)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.N_right_interface), self.N_right_interface)) + \
+                (self.beta*((self.material.E*self.material.I)/self.function_space.elL) * \
+                    np.matmul(np.transpose(self.Nx_right_interface), self.Nx_right_interface))
+        pass
+
+    # Function to compute the system nodal forces
+    # Computed by approaching every node from the left side!!!
+    def compute_system_nodal_forces(self, f, system_unknowns, element_loads_info=None):
+        dofs = self.function_space.dof
+        dofspel = self.function_space.dof*self.function_space.npel
+        _, phixi_left_node = self.function_space.compute_lagrange_shapes(-1.0)
+        phix_left_node = phixi_left_node*(1.0/self.function_space.jacobian)
+        _, _, Nxixi_left_node, Nxixixi_left_node = self.function_space.compute_hermite_shapes(-1.0)
+        Nxx_left_node = Nxixi_left_node*((1.0/self.function_space.jacobian)**2.0)
+        Nxxx_left_node = Nxixixi_left_node*((1.0/self.function_space.jacobian)**3.0)
+        _, phixi_right_node = self.function_space.compute_lagrange_shapes(1.0)
+        phix_right_node = phixi_right_node*(1.0/self.function_space.jacobian)
+        _, _, Nxixi_right_node, Nxixixi_right_node = self.function_space.compute_hermite_shapes(1.0)
+        Nxx_right_node = Nxixi_right_node*((1.0/self.function_space.jacobian)**2.0)
+        Nxxx_right_node = Nxixixi_right_node*((1.0/self.function_space.jacobian)**3.0)
+        for i in range(0, self.function_space.E): # loop over the elements
+            global_element_dofs = self.function_space.global_connectivity[i:i+1].flatten()
+            element_unknowns = system_unknowns[global_element_dofs]
+            global_element_dofs_left_node = global_element_dofs[0:dofs]
+            global_element_dofs_right_node = global_element_dofs[dofs:dofspel]
+            # forces at the left node
+            ux_left_node = np.matmul(phix_left_node, element_unknowns)
+            wxx_left_node = np.matmul(Nxx_left_node, element_unknowns)
+            wxxx_left_node = np.matmul(Nxxx_left_node, element_unknowns)
+            axial_force_left_node = self.material.E*self.material.A*ux_left_node
+            shear_force_left_node = -self.material.E*self.material.I*wxxx_left_node
+            bending_moment_left_node = -self.material.E*self.material.I*wxx_left_node
+            f[global_element_dofs_left_node[0:1]] += axial_force_left_node
+            f[global_element_dofs_left_node[1:2]] += shear_force_left_node
+            f[global_element_dofs_left_node[2:3]] += bending_moment_left_node
+            # forces at the right node
+            ux_right_node = np.matmul(phix_right_node, element_unknowns)
+            wxx_right_node = np.matmul(Nxx_right_node, element_unknowns)
+            wxxx_right_node = np.matmul(Nxxx_right_node, element_unknowns)
+            axial_force_right_node = self.material.E*self.material.A*ux_right_node
+            shear_force_right_node = -self.material.E*self.material.I*wxxx_right_node
+            bending_moment_right_node = -self.material.E*self.material.I*wxx_right_node
+            f[global_element_dofs_right_node[0:1]] += axial_force_right_node
+            f[global_element_dofs_right_node[1:2]] += shear_force_right_node
+            f[global_element_dofs_right_node[2:3]] += bending_moment_right_node
+        pass
