13f5795564ef91c55fabcc75a995560812d58cb3
Sai Kubair Kota <s.k.kota@tudelft.nl>
2025-06-27 10:16:40 +0200

    FunctionSpace: disabled lifting shape functions in ShearFlexibleGeometricallyFunctionSpace for testing purposes

--- lib/beamit/FunctionSpace.py
+++ lib/beamit/FunctionSpace.py
@@ -495,14 +495,17 @@ class ShearFlexibleGeometricallyExactFunctionSpace(FunctionSpace):
             shape_functions: The lifting shape functions evaluated at the point xi
         """
         # linear lifting shapes
-        k01 = (1.0-(3.0*xi))/4.0
-        k12 = (1.0+(3.0*xi))/4.0
+        # k01 = (1.0-(3.0*xi))/4.0
+        # k12 = (1.0+(3.0*xi))/4.0
+        k01 = 0.0
+        k12 = 0.0
 
         # elemental lifting shape function matrix
         lifting_shape_functions = np.array([[k01, 0.0, 0.0, k12, 0.0, 0.0],
                                             [0.0, k01, 0.0, 0.0, k12, 0.0],
                                             [0.0, 0.0, k01, 0.0, 0.0, k12]])
-        return lifting_shape_functions 
+
+        return lifting_shape_functions
 
     def discretize(self):
         """
