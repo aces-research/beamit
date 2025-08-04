612318870a7b12e0ca84c3ddfe60cd2cd184098c
Sai Kubair Kota <s.k.kota@tudelft.nl>
2025-08-04 09:29:07 +0200

    System: added docstrings and made some other minor edits

--- lib/beamit/System.py
+++ lib/beamit/System.py
@@ -8,6 +8,15 @@ from beamit.WeakForm.ShearFlexibleGeometricallyExactWeakForm import *
 class System:
 
     def __init__(self, function_space, material, betaP = 10.0, betaT = 10.0):
+        """
+        Initialize the system with the given function space, material, and DG penalty parameters.
+
+        Parameters:
+            function_space: The function space containing the geometrical information.
+            material: The material properties.
+            betaP: DG penalty parameter for translational compatibility (default is 10.0).
+            betaT: DG penalty parameter for rotational compatibility (default is 10.0).
+        """
         # use TFKL geometrically exact weak form for TFKL geometrically exact function space
         if (type(function_space) == TFKLGeometricallyExactFunctionSpace):
             if (function_space.discretization_type == "CG"):
@@ -55,34 +64,77 @@ class System:
                 [self.weak_form.function_space.N, self.weak_form.function_space.dof])
             self.state[:, 0:3] = self.weak_form.function_space.nodes
         # the internal forces of the system
-        self.internal_forces = np.zeros([self.weak_form.function_space.N, self.weak_form.function_space.dof])
+        self.internal_forces = np.zeros(
+            [self.weak_form.function_space.N, self.weak_form.function_space.dof])
         # the number of equations
         self.nequations = self.weak_form.function_space.N*self.weak_form.function_space.dof
 
     def assemble_stiffness(self, A, solution, nodal_loads, element_loads=None):
+        """
+        Assemble the stiffness matrix for the system.
+
+        Parameters:
+            A: The stiffness matrix to be assembled.
+            solution: The current solution vector.
+            nodal_loads: Nodal loads to be applied.
+            element_loads: Element loads to be applied (optional).
+        """
         self.weak_form.compute_system_stiffness(A, solution, nodal_loads, element_loads)
 
-    def assemble_residual(self, f, solution, nodal_loads, element_loads=None, update_internal=False):
+    def assemble_residual(self, f, solution, nodal_loads, element_loads=None, 
+                          update_internal=False):
+        """
+        Assemble the residual vector for the system.
+
+        Parameters:
+            f: The residual vector to be assembled.
+            solution: The current solution vector.
+            nodal_loads: Nodal loads to be applied.
+            element_loads: Element loads to be applied (optional).
+            update_internal: Whether to update the internal variables (default is False).
+        """
         self.weak_form.compute_system_residual(
             f, solution, element_loads, update_internal)
         # add nodal loads to the residual
-        # Generally, the addition of nodal loads just involves a direction addition to the residual. 
-        # But for the TFKL geometrically exact weak form, a special treatment is needed which is 
-        # implemented in the following function in the respective weak form
+        # NOTE: Generally, the addition of nodal loads just involves a direction addition to the 
+        # residual. But for some weak forms, a special treatment is needed which is implemented in
+        # the following function in the respective weak form
         self.weak_form.add_nodal_loads_to_residual(f, solution, nodal_loads)
 
     def assemble_mass(self, M, solution):
+        """
+        Assemble the mass matrix for the system.
+
+        Parameters:
+            M: The mass matrix to be assembled.
+            solution: The current solution vector.
+        """
         self.weak_form.compute_system_mass(M, solution)
 
     def assemble(self, A, f, solution, nodal_loads, element_loads=None):
+        """
+        Assemble the stiffness matrix and residual vector for the system.
+
+        Parameters:
+            A: The stiffness matrix to be assembled.
+            f: The residual vector to be assembled.
+            solution: The current solution vector.
+            nodal_loads: Nodal loads to be applied.
+            element_loads: Element loads to be applied (optional).
+        """
         # assemble stiffness
         self.assemble_stiffness(A, solution, nodal_loads, element_loads)
 
         # assemble residual
         self.assemble_residual(f, solution, nodal_loads, element_loads)
 
-    # Function to update the variables in the system
     def update(self, solution):
+        """
+        Update the state variables of the system with the new solution.
+
+        Parameters:
+            solution: The new solution vector.
+        """
         # update the state of the system
         self.state = np.reshape(solution, [self.weak_form.function_space.N, \
                                 self.weak_form.function_space.dof])
@@ -91,4 +143,5 @@ class System:
         self.weak_form.compute_system_nodal_forces(
             internal_force_vector, solution, element_loads=None)
         self.internal_forces = np.reshape(internal_force_vector, [
-                                          self.weak_form.function_space.N, self.weak_form.function_space.dof])
+                                          self.weak_form.function_space.N, 
+                                          self.weak_form.function_space.dof])
