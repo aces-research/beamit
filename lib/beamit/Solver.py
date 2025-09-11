a4b40290e53e356bf88909cf5f1cc4aa3f15c6cc
Sai Kubair Kota <s.k.kota@tudelft.nl>
2025-09-11 12:54:11 +0200

    Solver: corrected basic bug where mass matrix was not reset to zero before assembly in SFGE beams
    We forgot to reset the mass matrix to zero before assembling it in the case of shear flexible geometrically exact beams which added spurious mass to the system at every time step and resulted in blow up of the solution.

--- lib/beamit/Solver.py
+++ lib/beamit/Solver.py
@@ -521,7 +521,7 @@ class ExplicitNewmarkSolver(DynamicSolver):
         # compute the lumped mass
         self.system.assemble_mass(self.M)
         self.lumpedMass = (np.diag(self.M)).reshape([-1, 1])
-        # damping factor
+        # damping coefficient
         self.__damping = 0.0
 
     def compute_system_frequencies(self):
@@ -684,6 +684,7 @@ class ExplicitNewmarkSolver(DynamicSolver):
         # or ShearFlexibleGeometricallyExactWeakFormDG
         if ((type(self.system.weak_form) == ShearFlexibleGeometricallyExactWeakFormCG) or
             (type(self.system.weak_form) == ShearFlexibleGeometricallyExactWeakFormDG)):
+            self.M.fill(0.0)
             self.system.assemble_mass(
                 self.M, lump=True, dt=dt, system_velocities=self.velocity, residual_vector=self.f)
             self.lumpedMass = (np.diag(self.M)).reshape([-1, 1])
