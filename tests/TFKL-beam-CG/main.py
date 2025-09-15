ae22230ffa0e4946b222537b8871c9f5973251f0
Sai Kubair Kota <s.k.kota@tudelft.nl>
2025-09-15 17:10:55 +0200

    tests & examples: removed few unnecessary tests & examples; added assertions in TFKL-beam-CG main test

--- tests/TFKL-beam-CG/main.py
+++ tests/TFKL-beam-CG/main.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 import copy
 
-def static_main():
+def run_analysis(load_case):
 
     # density of the material
     rho = 7850.0
@@ -43,8 +43,7 @@ def static_main():
     bcvalues = np.zeros([function_space.N, function_space.dof])
 
     # the load case and output
-    load_case = 4
-    load_steps = 1000
+    load_steps = 100
     save_step = 1
 
     # applied loads and tolerances
@@ -53,6 +52,7 @@ def static_main():
     PERTURB_FORCE = 100.0
     DISP_CB = -0.01
     SPATIAL_TOLERANCE = 1.0E-05
+    NUMERICAL_TOLERANCE = 1.0E-05
 
     # clamp left node (a clamp fixes both the position and the direction of the tangent)
     # Note that the tangent vector r' is a unit vector if and only if the axial strain is zero.
@@ -107,19 +107,7 @@ def static_main():
                 # apply force at the center
                 elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
                     bcvalues[i, 1] = FORCE_Y
-            elif (load_case == 3): # SIMPLY SUPPORTED BEAM WITH POINT MOMENT AT THE CENTER
-                # pin at left end
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                # roller at the right end
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 1:3] = 1
-                    bcvalues[i, 1:3] = initial_state[i, 1:3]
-                # apply moment at the center
-                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 5] = MOMENT_Z
-            elif (load_case == 4): # MODE-I COLUMN BUCKLING WITH DISPLACEMENT CONTROL LOADING
+            elif (load_case == 3): # MODE-I COLUMN BUCKLING WITH DISPLACEMENT CONTROL LOADING
                 # pin at left end
                 if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
                     bctypes[i, 0:3] = 1
@@ -145,7 +133,7 @@ def static_main():
     PostProcess.write_output_vtk("./VTK/output-0", system)
 
     # incremental computation of the load path
-    if (load_case == 4):
+    if (load_case == 3):
         for i in range(0, load_steps):
             print("\nCurrent load step:", i+1,"out of", load_steps, "load steps.")
             load_level = (i+1)/load_steps
@@ -165,183 +153,41 @@ def static_main():
         solver.solve(Nmax = 20, tol = 1.0E-03)
         PostProcess.write_output_vtk("./VTK/output-1", system)
 
-def dynamic_main():
+    # asserts for the different load cases
+    if (load_case == 0): # SIMPLY SUPPORTED BEAM WITH POINT LOAD
+        # maximum deflection at the center
+        assert(np.abs(system.state[function_space.N//2, 1] - 
+            (FORCE_Y*L**3/(12*E*np.pi*R**4))) < NUMERICAL_TOLERANCE)
+    elif (load_case == 1): # CANTILEVER BEAM WITH POINT MOMENT
+        # maximum deflection at the right end
+        assert(np.abs(system.state[function_space.N-1, 1] - 
+            (MOMENT_Z*L**2/(0.50*E*np.pi*R**4))) < NUMERICAL_TOLERANCE)
+    elif (load_case == 2): # FIXED-FIXED BEAM WITH POINT LOAD
+        # maximum deflection at the center
+        assert(np.abs(system.state[function_space.N//2, 1] - 
+            (FORCE_Y*L**3/(48*E*np.pi*R**4))) < NUMERICAL_TOLERANCE)
+    elif (load_case == 3): # MODE-I COLUMN BUCKLING WITH DISPLACEMENT_CONTROL LOADING
+        # axial reaction at the supported end
+        EI = (E*np.pi*R**4)/4.0
+        Pcr = (np.pi**2*EI)/(L**2)
+        axial_reaction = -system.internal_forces[function_space.N-1, 0]
+        assert (np.abs(axial_reaction - Pcr) /
+                Pcr < NUMERICAL_TOLERANCE*1.0E03)
 
-    # density of the material
-    rho = 7850.0
-    # elastic modulus of beam
-    E = 2.0E11
-    # radius of the beam
-    R = 1.0E-01
-    # physical information (material parameters)
-    material = Material.TFKLMaterial(rho, E, R)
+def test_load_case_0():
+    run_analysis(load_case=0)
 
-    # length of beam
-    L = 10.0
-    # number of elements
-    Nel = 10
-    # geometric information (domain, no. of elements)
-    function_space = FunctionSpace.TFKLGeometricallyExactFunctionSpace(0, L, Nel, discretization_type = "CG")
-    function_space.discretize()
-    # to avoid creating reference to the object attributes
-    # a better idea is to create private attributes and use accessors
-    nodal_coordinates = copy.deepcopy(function_space.nodes)
+def test_load_case_1():
+    run_analysis(load_case=1)
 
-    # a system binding the function_space (math) and the material (physics) 
-    system = System.System(function_space, material)
-    # to avoid creating reference to the object attributes
-    initial_state = copy.deepcopy(system.state)
+def test_load_case_2():
+    run_analysis(load_case=2)
 
-    # the solver
-    solver = Solver.NewmarkSolver(system)
+def test_load_case_3():
+    run_analysis(load_case=3)
 
-    # boundary condition types (0 = Neumann, 1 = Dirichlet) matrix
-    bctypes = np.zeros([function_space.N, function_space.dof], dtype=np.int64)
-    # boundary condition values matrix
-    bcvalues = np.zeros([function_space.N, function_space.dof])
-
-    # the load case and time details
-    load_case = 5
-    dt = 0.001
-    time_steps = 100
-    save_time = 1
-    
-    # applied loads and tolerances
-    FORCE_X_CB = -5.0E05
-    FORCE_Y = -1.0E04
-    MOMENT_Z = 1.0E04
-    PERTURB_FORCE = 100.0
-    PERTURB_DISP = 0.0
-    SPATIAL_TOLERANCE = 1.0E-05
-
-    # generate initial state of the system
-    if (load_case == 5): # MODE-I COLUMN BUCKLING
-        for i in range(0, nodal_coordinates.shape[0]): # loop over the nodes
-            x_coord = nodal_coordinates[i, 0]
-            y_coord = nodal_coordinates[i, 1]
-            z_coord = nodal_coordinates[i, 2]
-            # perturbation displacement at the center
-            if ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                initial_state[i, 1] += PERTURB_DISP
-      
-    # linear displacement signal function
-    def linear_displacement_signal(time):
-        return time
-    
-    # function to apply BCs
-    def update_BCs(bctypes, bcvalues, load_case, load_level, simulation_time):
-        for i in range(0, nodal_coordinates.shape[0]): # loop over the nodes
-            x_coord = nodal_coordinates[i, 0]
-            y_coord = nodal_coordinates[i, 1]
-            z_coord = nodal_coordinates[i, 2]
-            if (load_case == 0): # CANTILEVER BEAM WITH POINT LOAD AT THE RIGHT END
-                # clamp left node
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bctypes[i, 4:6] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                    bcvalues[i, 4:6] = initial_state[i, 4:6]
-                # apply force on right node
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 1] = load_level*FORCE_Y
-            elif (load_case == 1): # CANTILEVER BEAM WITH POINT MOMENT AT THE RIGHT END
-                # clamp left node
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bctypes[i, 4:6] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                    bcvalues[i, 4:6] = initial_state[i, 4:6]
-                # apply moment on right node
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 5] = load_level*MOMENT_Z
-            elif (load_case == 2): # FIXED-FIXED BEAM WITH POINT LOAD AT THE CENTER
-                # clamp at left end
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bctypes[i, 4:6] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                    bcvalues[i, 4:6] = initial_state[i, 4:6]
-                # clamp at right end
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bctypes[i, 4:6] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                    bcvalues[i, 4:6] = initial_state[i, 4:6]
-                # apply force at the center
-                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 1] = load_level*FORCE_Y
-            elif (load_case == 3): # SIMPLY SUPPORTED BEAM WITH POINT MOMENT AT THE CENTER
-                # pin at left end
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                # roller at the right end
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 1:3] = 1
-                    bcvalues[i, 1:3] = initial_state[i, 1:3]
-                # apply moment at the center
-                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 5] = load_level*MOMENT_Z
-            elif (load_case == 4): # SIMPLY SUPPORTED BEAM WITH AXIAL FORCE AT RIGHT END AND PERTURBATION FORCE AT THE CENTER
-                # pin at left end
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                # roller and axial load at right end
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 1:3] = 1
-                    bcvalues[i, 1:3] = initial_state[i, 1:3]
-                    bcvalues[i, 0] = load_level*FORCE_X_CB
-                # perturbation force at the center
-                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 1] = PERTURB_FORCE
-            elif (load_case == 5): # MODE-I COLUMN BUCKLING WITH DISPLACEMENT CONTROL LOADING
-                # pin at left end
-                if ((x_coord <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bcvalues[i, 0:3] = initial_state[i, 0:3]
-                # roller and axial displacement at right end
-                elif ((abs(x_coord - L) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bctypes[i, 0:3] = 1
-                    bcvalues[i, 0] = initial_state[i, 0] - linear_displacement_signal(simulation_time)
-                    bcvalues[i, 1:3] = initial_state[i, 1:3]
-                # perturbation force at the center
-                elif ((abs(x_coord - L/2.0) <= SPATIAL_TOLERANCE) and (y_coord <= SPATIAL_TOLERANCE) and (z_coord <= SPATIAL_TOLERANCE)):
-                    bcvalues[i, 1] = PERTURB_FORCE
-
-    # set boundary and initial conditions
-    update_BCs(bctypes, bcvalues, load_case, load_level = 0.0, simulation_time = 0.0)
-    solver.set_boundary_conditions(bctypes, bcvalues)
-    # to avoid creating reference to the object attributes
-    initial_position = copy.deepcopy(initial_state)
-    initial_velocity = np.zeros([function_space.N, function_space.dof])
-    solver.set_initial_conditions(initial_position, initial_velocity)
-    
-    # create a VTK directory or clear it
-    if not os.path.isdir("VTK"):
-        os.mkdir("VTK")
-    else:
-        for item in os.listdir("VTK"):
-            os.remove(os.path.join("VTK", item))
-    
-    # write the initial displacements
-    output_file = "./VTK/output-0"
-    PostProcess.write_output_vtk(output_file, system)
-    
-    # solve the nonlinear dynamic problem and update the nodal position in system
-    simulation_time = 0.0
-    for i in range(0, time_steps):
-        print("\nCurrent time step:", i+1,"out of", time_steps, "time steps.")
-        load_level = (i+1)/time_steps
-        simulation_time += dt
-        update_BCs(bctypes, bcvalues, load_case, load_level, simulation_time)
-        # apply the boundary conditions
-        solver.set_boundary_conditions(bctypes, bcvalues)
-        solver.solve(dt, Nmax = 20, tol = 1.0E-03)
-        if ((i+1) % save_time == 0):
-            output_file = "./VTK/output-" + str(i+1)
-            PostProcess.write_output_vtk(output_file, system)
-
-# run main functions
-static_main()
-# dynamic_main()
+if __name__ == "__main__":
+    test_load_case_0()
+    test_load_case_1()
+    test_load_case_2()
+    test_load_case_3()
