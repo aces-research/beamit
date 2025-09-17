WeakForm: corrected calculation of internal forces based on its new size in system
Corrected the calculation of internal forces in weak form classes so that each element writes its own internal forces i.e. in a DG fashion instead of accumulating them based on the global DOFs as done previously which would result in incorrect assembly for CG discretizations.

lib/beamit/WeakForm/EulerBernoulliWeakForm.py
@@ -108,11 +108,14 @@ class EulerBernoulliWeakFormCG(WeakForm):
         _, _, Nxixi_right_node, Nxixixi_right_node = self.function_space.compute_hermite_shapes(1.0)
         Nxx_right_node = Nxixi_right_node*((1.0/self.function_space.jacobian)**2.0)
         Nxxx_right_node = Nxixixi_right_node*((1.0/self.function_space.jacobian)**3.0)
+        internal_force_dofs = np.arange(
+            0, self.function_space.E*self.function_space.npel*dofs, 1, dtype=np.int64)
         for i in range(0, self.function_space.E): # loop over the elements
             global_element_dofs = self.function_space.global_connectivity[i:i+1].flatten()
             element_unknowns = system_unknowns[global_element_dofs]
-            global_element_dofs_left_node = global_element_dofs[0:dofs]
-            global_element_dofs_right_node = global_element_dofs[dofs:dofspel]
+            internal_force_element_dofs = internal_force_dofs[i*dofspel:(i+1)*dofspel]
+            internal_force_element_dofs_left_node = internal_force_element_dofs[0:dofs]
+            internal_force_element_dofs_right_node = internal_force_element_dofs[dofs:dofspel]
             # forces at the left node
             ux_left_node = np.matmul(phix_left_node, element_unknowns)
             wxx_left_node = np.matmul(Nxx_left_node, element_unknowns)
@@ -120,9 +123,9 @@ class EulerBernoulliWeakFormCG(WeakForm):
             axial_force_left_node = self.material.E*self.material.A*ux_left_node
             shear_force_left_node = -self.material.E*self.material.I*wxxx_left_node
             bending_moment_left_node = -self.material.E*self.material.I*wxx_left_node
-            f[global_element_dofs_left_node[0:1]] += axial_force_left_node
-            f[global_element_dofs_left_node[1:2]] += shear_force_left_node
-            f[global_element_dofs_left_node[2:3]] += bending_moment_left_node
+            f[internal_force_element_dofs_left_node[0:1]] += axial_force_left_node
+            f[internal_force_element_dofs_left_node[1:2]] += shear_force_left_node
+            f[internal_force_element_dofs_left_node[2:3]] += bending_moment_left_node
             # forces at the right node
             ux_right_node = np.matmul(phix_right_node, element_unknowns)
             wxx_right_node = np.matmul(Nxx_right_node, element_unknowns)
@@ -130,9 +133,9 @@ class EulerBernoulliWeakFormCG(WeakForm):
             axial_force_right_node = self.material.E*self.material.A*ux_right_node
             shear_force_right_node = -self.material.E*self.material.I*wxxx_right_node
             bending_moment_right_node = -self.material.E*self.material.I*wxx_right_node
-            f[global_element_dofs_right_node[0:1]] += axial_force_right_node
-            f[global_element_dofs_right_node[1:2]] += shear_force_right_node
-            f[global_element_dofs_right_node[2:3]] += bending_moment_right_node
+            f[internal_force_element_dofs_right_node[0:1]] += axial_force_right_node
+            f[internal_force_element_dofs_right_node[1:2]] += shear_force_right_node
+            f[internal_force_element_dofs_right_node[2:3]] += bending_moment_right_node
 
 
 class EulerBernoulliWeakFormDG(EulerBernoulliWeakFormCG):

lib/beamit/WeakForm/ShearFlexibleGeometricallyExactWeakForm.py
@@ -731,12 +731,15 @@ class ShearFlexibleGeometricallyExactWeakFormCG(WeakForm):
         """
         dofs = self.function_space.dof
         dofspel = self.function_space.dof*self.function_space.npel
+        internal_force_dofs = np.arange(
+            0, self.function_space.E*self.function_space.npel*dofs, 1, dtype=np.int64)
         for i in range(0, self.function_space.E):
             global_element_dofs = self.function_space.global_connectivity[i:i+1].flatten()
             # NOTE: Here we assume that there are only two nodes per element!!!
-            global_element_dofs_left_node = global_element_dofs[0:dofs]
-            global_element_dofs_right_node = global_element_dofs[dofs:dofspel]
             element_unknowns = system_unknowns[global_element_dofs]
+            internal_force_element_dofs = internal_force_dofs[i*dofspel:(i+1)*dofspel]
+            internal_force_element_dofs_left_node = internal_force_element_dofs[0:dofs]
+            internal_force_element_dofs_right_node = internal_force_element_dofs[dofs:dofspel]
             nodal_orientations = element_unknowns[self.function_space.local_rotational_dofs].reshape(
                 -1, self.function_space.dim)
             nodal_curvatures = np.stack(
@@ -745,14 +748,14 @@ class ShearFlexibleGeometricallyExactWeakFormCG(WeakForm):
             internal_forces, internal_moments = self._compute_internal_forces_and_moments(
                 i, element_unknowns, nodal_orientations, nodal_curvatures, location="Nodes")
             # assemble the internal forces at the left node
-            f[global_element_dofs_left_node[0:int(
+            f[internal_force_element_dofs_left_node[0:int(
                 dofs/2)]] += internal_forces[0, ...]
-            f[global_element_dofs_left_node[int(
+            f[internal_force_element_dofs_left_node[int(
                 dofs/2):dofs]] += internal_moments[0, ...]
             # assemble the internal forces at the right node
-            f[global_element_dofs_right_node[0:int(
+            f[internal_force_element_dofs_right_node[0:int(
                 dofs/2)]] += internal_forces[1, ...]
-            f[global_element_dofs_right_node[int(
+            f[internal_force_element_dofs_right_node[int(
                 dofs/2):dofs]] += internal_moments[1, ...]
 
     def __compute_element_rotational_mass(self, e, dt, element_angular_velocities):

lib/beamit/WeakForm/TFKLGeometricallyExactWeakForm.py
@@ -193,19 +193,24 @@ class TFKLGeometricallyExactWeakFormCG(WeakForm):
         """
         dofs = self.function_space.dof
         dofspel = self.function_space.dof*self.function_space.npel
-        _, Nxi_left_node, Nxixi_left_node, Nxixixi_left_node = self.function_space.compute_shapes(-1.0)
+        _, Nxi_left_node, Nxixi_left_node, Nxixixi_left_node = \
+            self.function_space.compute_shapes(-1.0)
         Np_left_node = Nxi_left_node*(1.0/self.function_space.jacobian)
         Npp_left_node = Nxixi_left_node*((1.0/self.function_space.jacobian)**2.0)
         Nppp_left_node = Nxixixi_left_node*((1.0/self.function_space.jacobian)**3.0)
-        _, Nxi_right_node, Nxixi_right_node, Nxixixi_right_node = self.function_space.compute_shapes(1.0)
+        _, Nxi_right_node, Nxixi_right_node, Nxixixi_right_node = \
+            self.function_space.compute_shapes(1.0)
         Np_right_node = Nxi_right_node*(1.0/self.function_space.jacobian)
         Npp_right_node = Nxixi_right_node*((1.0/self.function_space.jacobian)**2.0)
         Nppp_right_node = Nxixixi_right_node*((1.0/self.function_space.jacobian)**3.0)
+        internal_force_dofs = np.arange(
+            0, self.function_space.E*self.function_space.npel*dofs, 1, dtype=np.int64)
         for i in range(0, self.function_space.E): # loop over the elements
             global_element_dofs = self.function_space.global_connectivity[i:i+1].flatten()
             element_unknowns = system_unknowns[global_element_dofs]
-            global_element_dofs_left_node = global_element_dofs[0:dofs]
-            global_element_dofs_right_node = global_element_dofs[dofs:dofspel]
+            internal_force_element_dofs = internal_force_dofs[i*dofspel:(i+1)*dofspel]
+            internal_force_element_dofs_left_node = internal_force_element_dofs[0:dofs]
+            internal_force_element_dofs_right_node = internal_force_element_dofs[dofs:dofspel]
             rp_left_node = np.matmul(Np_left_node, element_unknowns)
             rp_right_node = np.matmul(Np_right_node, element_unknowns)
             rpp_left_node = np.matmul(Npp_left_node, element_unknowns)
@@ -214,19 +219,29 @@ class TFKLGeometricallyExactWeakFormCG(WeakForm):
             rppp_right_node = np.matmul(Nppp_right_node, element_unknowns)
             rp_left_node_L2 = np.linalg.norm(rp_left_node, ord=2, axis=0, keepdims=True)
             rp_right_node_L2 = np.linalg.norm(rp_right_node, ord=2, axis=0, keepdims=True)
-            t1_left_node, _, _, _, t5_left_node = self._compute_residual_vectors(rp_left_node, rpp_left_node, rppp_left_node)
-            t1_right_node, _, _, _, t5_right_node = self._compute_residual_vectors(rp_right_node, rpp_right_node, rppp_right_node)
+            t1_left_node, _, _, _, t5_left_node = \
+                self._compute_residual_vectors(rp_left_node, rpp_left_node, rppp_left_node)
+            t1_right_node, _, _, _, t5_right_node = \
+                self._compute_residual_vectors(rp_right_node, rpp_right_node, rppp_right_node)
             # forces at the nodes
             if (element_loads == None): # No element loads
-                forces_left_node = (self.material.E*self.material.A*t1_left_node) + (self.material.E*self.material.I*t5_left_node)
-                forces_right_node = (self.material.E*self.material.A*t1_right_node) + (self.material.E*self.material.I*t5_right_node)
+                forces_left_node = (self.material.E*self.material.A*t1_left_node) + \
+                    (self.material.E*self.material.I*t5_left_node)
+                forces_right_node = (self.material.E*self.material.A*t1_right_node) + \
+                    (self.material.E*self.material.I*t5_right_node)
             # moments at the nodes
-            moments_left_node = self.material.E*self.material.I*(np.cross(rp_left_node, rpp_left_node, axis=0)/(rp_left_node_L2**2.0))
-            moments_right_node = self.material.E*self.material.I*(np.cross(rp_right_node, rpp_right_node, axis=0)/(rp_right_node_L2**2.0))
-            f[global_element_dofs_left_node[0:int(dofs/2)]] += forces_left_node
-            f[global_element_dofs_left_node[int(dofs/2):dofs]] += moments_left_node
-            f[global_element_dofs_right_node[0:int(dofs/2)]] += forces_right_node
-            f[global_element_dofs_right_node[int(dofs/2):dofs]] += moments_right_node
+            moments_left_node = self.material.E*self.material.I * \
+                (np.cross(rp_left_node, rpp_left_node, axis=0)/(rp_left_node_L2**2.0))
+            moments_right_node = self.material.E*self.material.I * \
+                (np.cross(rp_right_node, rpp_right_node, axis=0)/(rp_right_node_L2**2.0))
+            f[internal_force_element_dofs_left_node[0:int(
+                dofs/2)]] += forces_left_node
+            f[internal_force_element_dofs_left_node[int(
+                dofs/2):dofs]] += moments_left_node
+            f[internal_force_element_dofs_right_node[0:int(
+                dofs/2)]] += forces_right_node
+            f[internal_force_element_dofs_right_node[int(
+                dofs/2):dofs]] += moments_right_node
 
     # Function to compute the system mass
     def compute_system_mass(self, M, lump=True, **kwargs):