1abbf7949e535a474f0b14e3acca13c5abdb1b97
Sai Kubair Kota <s.k.kota@tudelft.nl>
2025-09-22 16:33:22 +0200

    Material: modified damage initiation criterion in ShearFlexibleCohesiveInterfaceMaterial to consider effective force on either sides of interface

--- lib/beamit/Material.py
+++ lib/beamit/Material.py
@@ -171,9 +171,9 @@ class ShearFlexibleCohesiveInterfaceMaterial(ShearFlexibleMaterial):
         effective_force_right_interface = self.compute_effective_force(
             psi_left_interface, psi_right_interface, forces_right_interface,
             forces_right_interface, moments_right_interface, moments_right_interface)
-        # if the effective force at the interface (on both sides) satisfies the damage 
+        # if the effective force at the interface (on either side) satisfies the damage
         # initiation criterion
-        if ((effective_force_left_interface / self.fc >= 1.0) and
+        if ((effective_force_left_interface / self.fc >= 1.0) or
             (effective_force_right_interface / self.fc >= 1.0)):
             return True
         else:
